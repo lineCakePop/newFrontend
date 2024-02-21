132580f4a95331a7586dacab9044b83fa5fa2d4e
Paranchai <paranchai.wt@gmail.com>
2024-02-21 19:25:46 +0700

    add Route

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
-import liff from "@line/liff";
-import "./App.css";
-import ItemInformation from "./pages/ItemInformation";
-import axios from "axios";
+import { Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
+import liff from "@line/liff"
+import "./App.css"
+import ItemInformation from "./pages/ItemInformation"
+import axios from "axios"
+import AddBirthday from "./pages/AddBirthday"
 
-const LiffId = process.env.REACT_APP_LIFF_ID;
+const LiffId = process.env.REACT_APP_LIFF_ID
 
 function App() {
-  useEffect(() => {
-    // liffCheck();
-  }, []);
+    useEffect(() => {
+        // liffCheck();
+    }, [])
 
-  // auth
-  const liffCheck = async () => {
-    // const isInClient = await liff.isInClient();
-    await liff.init({
-      liffId: LiffId,
-      withLoginOnExternalBrowser: true,
-    });
-    const idToken = await liff.getIDToken();
-    console.log(idToken);
-    const response = await axios.post(
-      "https://api.line.me/oauth2/v2.1/verify",
-      { id_token: idToken, client_id: 2003293436 }
-    );
-    console.log(response);
-  };
+    // auth
+    const liffCheck = async () => {
+        // const isInClient = await liff.isInClient();
+        await liff.init({
+            liffId: LiffId,
+            withLoginOnExternalBrowser: true,
+        })
+        const idToken = await liff.getIDToken()
+        console.log(idToken)
+        const response = await axios.post(
+            "https://api.line.me/oauth2/v2.1/verify",
+            { id_token: idToken, client_id: 2003293436 }
+        )
+        console.log(response)
+    }
 
-  return (
-    <>
-      <Routes>
-        <Route path="*" element={<ItemInformation />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="*" element={<ItemInformation />} />
+                <Route path="/AddBirthday" element={<AddBirthday />} />
+            </Routes>
+        </>
+    )
 }
 
-export default App;
+export default App
