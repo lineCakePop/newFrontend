1fa3ede16f6e253cac2e694ffebc02bb10799441
kanpech-t <kankasem3002@gmail.com>
2024-03-08 14:35:28 +0700

    fix birth text

--- src/pages/addBirthday/index.js
+++ src/pages/addBirthday/index.js
@@ -311,8 +311,7 @@ const AddBirthday = () => {
 
             <div className="mt-[16px] text-[12px] leading-[15.6px] text-[#555555]">
               If you choose to notify your birthday, we’ll notify your birthday
-              to your friends who also added Line Birthday on your birthday and
-              1 day before.
+              to your friends who also added "Line Birthday" on your birthday.
             </div>
           </div>
         </div>
