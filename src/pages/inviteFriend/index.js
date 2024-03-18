87d063ede21ee3576dcbdb907ab2c3f75f9a8f44
kanpech-t <kankasem3002@gmail.com>
2024-03-18 17:49:48 +0700

    add welcome message

--- src/pages/inviteFriend/index.js
+++ src/pages/inviteFriend/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import axios from "axios";
 
@@ -9,10 +9,16 @@ import { AuthContext } from "../../context/AuthContext";
 
 import liff from "@line/liff";
 
+import { ReactComponent as Welcome } from "../../icons/welcome.svg";
+
+import { LOADING, SUCCESS } from "../../utils/const";
+
 function InviteFriend() {
   const { idToken } = useContext(AuthContext);
 
   const { inviteCode } = useParams();
+
+  const [status, setStatus] = useState(LOADING);
 
   useEffect(() => {
     if (idToken !== "" && inviteCode !== "") {
@@ -20,26 +26,26 @@ function InviteFriend() {
     }
   }, [idToken, inviteCode]);
 
-  console.log(inviteCode);
-
   const sendInvite = async () => {
     try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_PROXY}/user/inviteFriend`,
-        {
-          inviteCode: inviteCode,
-          tokenId: idToken,
-        }
-      );
-      liff.closeWindow();
+      await axios.post(`${process.env.REACT_APP_API_PROXY}/user/inviteFriend`, {
+        inviteCode: inviteCode,
+        tokenId: idToken,
+      });
+      setStatus(SUCCESS);
     } catch (err) {
       console.log(err);
+      liff.closeWindow();
     }
   };
 
   return (
     <div className="h-[100dvh] flex justify-center items-center">
-      <img src={loadingGif} alt="loading" />
+      {status !== LOADING ? (
+        <img src={loadingGif} alt="loading" />
+      ) : (
+        <Welcome />
+      )}
     </div>
   );
 }
