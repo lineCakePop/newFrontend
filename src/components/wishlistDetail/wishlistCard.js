35980257ef62e0330794782acc66cf5d6e6ed5ab
Paranchai <paranchai.wt@gmail.com>
2024-03-27 12:45:11 +0700

    add link to product

--- src/components/wishlistDetail/wishlistCard.js
+++ src/components/wishlistDetail/wishlistCard.js
@@ -11,16 +11,26 @@ const WishlistCard = ({
   wishlist,
   haveDiscount,
   discountPrice,
+  linkToProduct,
 }) => {
+  const handleLinkToProduct = () => {
+    window.location.href = linkToProduct;
+  };
   return (
     <div className="w-[328px] h-[108px] flex gap-[10px]">
       {/* img */}
-      <div className="h-[108px] w-[106px] flex justify-center items-center overflow-hidden rounded">
-        <img src={productPicture} alt="product picture" className="w-[106px]" />
+      <div
+        className="h-[108px] w-[106px] flex justify-center items-center overflow-hidden rounded"
+        onClick={handleLinkToProduct}
+      >
+        <img src={productPicture} alt="product" className="w-[106px]" />
       </div>
       {/* detail */}
       <div>
-        <div className="font-semibold text-[14px] leading-[18.2px] w-[180px] truncate h-[18.2px] ">
+        <div
+          className="font-semibold text-[14px] leading-[18.2px] w-[180px] truncate h-[18.2px] "
+          onClick={handleLinkToProduct}
+        >
           {productName}
         </div>
         <div className="leading-[18.2px] h-[18.2px]  font-normal text-[#555555] text-[14px] mt-[4px]">
